23136a0646ffbf2fd498fbae77b21b091d315438
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-03-27 20:51:45 -0300

    Update index.html

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,64 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
-    const questions = document.querySelectorAll('[data-faq-question]');
 
-    const heroSection = document.querySelector('.hero');
-    const alturaHero = heroSection.clientHeight;
-
-    window.addEventListener('scroll', function() {
-        const posicaoAtual = this.window.scrollY;
-
-        if (posicaoAtual < alturaHero) {
-            ocultaElementosDoHeader();
-        } else {
-            exibeElementosDoHeader();
-        }
-    })
-
-    // seçao de atraçoes, programaçao das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
-            removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active');
         })
-    }
-
-    for (let i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', AbreOuFechaResposta);
     }
 })
 
-function ocultaElementosDoHeader() {
-    const header = document.querySelector('header');
-    header.classList.add('header--is-hidden');
-}
-
-function exibeElementosDoHeader() {
-    const header = document.querySelector('header');
-    header.classList.remove('header--is-hidden');
-}
-
-function AbreOuFechaResposta(elemento) {
-    const classe = 'faq__questions__item--is-open';
-    const elementoPai = elemento.target.parentNode;
-
-    elementoPai.classList.toggle(classe);
-}
-
-function removeBotaoAtivo() {
-    const buttons = document.querySelectorAll('[data-bs-button]');
-
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active');
-    }
-}
-
-function escondeTodasAbas () {
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
